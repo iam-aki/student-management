6e9b4052cfd74fa0bcffc5a2f2369c77aa31a412
iam-aki <17708554+iam-aki@users.noreply.github.com>
2020-10-28 08:44:46 +0530

    Spring Initialised

---
+++ src/main/java/com/student/stdntmgmt/StdntMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.student.stdntmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdntMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdntMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/stdntmgmt/StdntMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.stdntmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdntMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
